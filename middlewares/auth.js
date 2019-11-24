1421ec4183c6ab93395648ce3c1cb8e526cdea87
platon <dazkza@gmail.com>
2019-11-24 15:20:25 +0300

    authorization fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,9 +6,10 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthError('Нужна авторизация'));
+  }
   const token = authorization.replace('Bearer ', '');
-
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
@@ -18,5 +19,5 @@ module.exports = (req, res, next) => {
   }
 
   req.user = payload;
-  next();
+  return next();
 };
